f2fd1588750db174d5bcc3d422b667877aee3726
Manan Kevadiya <kevadiyamanan@gmail.com>
2021-02-16 15:13:38 +0530

    Remove unnecessary dependency

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
-const normalize = require("normalize-url");
 
 const User = require("../../models/User");
 
